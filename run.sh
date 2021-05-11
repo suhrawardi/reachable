845f217c22e673c052e3528dbcda229b49730706
jarra <suhrawardi@gmail.com>
2021-05-11 07:18:18 +0200

    Trying to speed things up by pre-compiling

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-souffle -F./input -D./output reachable.dl
+# souffle -F./input -D./output reachable.dl
+
+souffle -g reachable.cpp reachable.dl
+clang++ -std=c++17 -O3 reachable.cpp -o reachable
+
+reachable
